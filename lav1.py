6f04fce7eaabf39c46b80636d1e1f1d7ac3875cf
Wqescs <146982081+Wqescs@users.noreply.github.com>
2024-06-22 10:50:43 +0300

    Add files via upload

---
+++ lav1.py
@@ -0,0 +1,26 @@
+opened = '{'
+closed = '}'
+meter1 = 0
+meter2 = 0
+slash = '/'
+
+with open('Lav1.txt', 'r') as file:
+    while True:
+        char = file.read(1)
+        # Открывая фигурная скобка, переключаем флаг
+        if char == opened:
+            meter1 += 1
+        # Закрывающую фигурная скобка, сбрасываем флаг
+        elif char == closed:
+            meter1 -= 1
+        elif char == slash:
+            meter2 += 1
+        elif char == '\n' and meter2 != 0:
+            meter2 -= 1
+
+        # Не внутри фигурных скобок, выводим символ
+        elif meter1 == 0 and meter2 == 0:
+            print(char, end='')
+        # Если конец файла, выходим из цикла
+        if not char:
+            break
